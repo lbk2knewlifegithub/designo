7208cbbc45b527314f4aee4b9f3eb0793ea37c23
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-04-15 21:29:18 +0700

    Fix okteto-pipeline.yaml

--- client/libs/environments/src/lib/environment.prod.ts
+++ client/libs/environments/src/lib/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // Api
   apiAuthUrl: 'https://api-auth-lbk2knewlifegithub.cloud.okteto.net',
-  apiImagesUrl: 'https://api-images-lemon-lbk2knewlifegithub.cloud.okteto.net',
+  apiImagesUrl: 'https://api-images-lbk2knewlifegithub.cloud.okteto.net',
   apiProductFeedbacksUrl:
     'https://api-product-feedbacks-lbk2knewlifegithub.cloud.okteto.net',
 
