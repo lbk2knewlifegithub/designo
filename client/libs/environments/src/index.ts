1e6235eb7b5c19eff490d5a47a9a69c1c4e18e3c
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-04-16 08:23:28 +0700

    Initital setup staging environment

--- client/libs/environments/src/index.ts
+++ client/libs/environments/src/index.ts
@@ -1,2 +1,3 @@
 export { environment as envDev } from './lib/environment';
 export { environment as envProd } from './lib/environment.prod';
+export { environment as envStage } from './lib/environment.stage';
