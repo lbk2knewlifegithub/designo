9258aec47c81e4f0055e0362e123137831db5376
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-04-17 11:34:11 +0700

    Create responsive tablet for contact page Designo

--- client/apps/designo/src/app/pages/contact/components/contact-form.component.ts
+++ client/apps/designo/src/app/pages/contact/components/contact-form.component.ts
@@ -4,40 +4,42 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'lbk-contact-form',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <section class="container bg-peach-200 py-[72px] text-white">
-      <div class="text-center">
-        <h2 class="text-lg">Contact Us</h2>
-        <p class="mt-6 text-sm">
-          Ready to take it to the next level? Let’s talk about your project or
-          idea and find out how we can help your business grow. If you are
-          looking for unique digital experiences that’s relatable to your users,
-          drop us a line.
-        </p>
-      </div>
-
-      <form class="mt-12 grid place-items-center">
-        <div class="grid gap-6 w-full">
-          <!-- Name -->
-          <input type="text" placeholder="Name" />
-          <!-- end Name -->
-
-          <!-- Email -->
-          <input type="email" placeholder="Email" />
-          <!-- end Email -->
-
-          <!-- Phone -->
-          <input type="text" placeholder="Phone" />
-          <!-- end Phone -->
-
-          <!-- Message -->
-          <textarea rows="3" placeholder="Your Message"></textarea>
-          <!-- end Message -->
+    <section class="container bg-peach-200  text-white md:bg-transparent">
+      <div class="py-[72px] md:rounded-lg md:bg-peach-200 md:px-14">
+        <div class="text-center md:text-left">
+          <h2 class="font-medium text-lg md:text-xl">Contact Us</h2>
+          <p class="mt-6 text-sm md:text-base">
+            Ready to take it to the next level? Let’s talk about your project or
+            idea and find out how we can help your business grow. If you are
+            looking for unique digital experiences that’s relatable to your
+            users, drop us a line.
+          </p>
         </div>
 
-        <!-- Submit Button -->
-        <button class="btn btn-white px-12 mt-10">SUBMIT</button>
-        <!-- end Submit Button -->
-      </form>
+        <form class="mt-12 grid place-items-center md:mt-10 md:place-items-end">
+          <div class="grid gap-6 w-full">
+            <!-- Name -->
+            <input type="text" placeholder="Name" />
+            <!-- end Name -->
+
+            <!-- Email -->
+            <input type="email" placeholder="Email" />
+            <!-- end Email -->
+
+            <!-- Phone -->
+            <input type="text" placeholder="Phone" />
+            <!-- end Phone -->
+
+            <!-- Message -->
+            <textarea rows="3" placeholder="Your Message"></textarea>
+            <!-- end Message -->
+          </div>
+
+          <!-- Submit Button -->
+          <button class="btn btn-white px-12 mt-10">SUBMIT</button>
+          <!-- end Submit Button -->
+        </form>
+      </div>
     </section>
   `,
   styles: [
