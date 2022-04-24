365a7d9486c5bfa5fff58e0952ce9c341a954e1e
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-04-24 11:40:56 +0700

    Update root route

--- api/frontendmentor/src/main.rs
+++ api/frontendmentor/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{web, App, HttpServer};
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Hello World!" })))
+    HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Frontendmentor Close" })))
         .bind(("0.0.0.0", 8083))?
         .run()
         .await
