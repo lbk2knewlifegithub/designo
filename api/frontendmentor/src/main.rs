fb214b2336fd48080373c91d88cc0d913587a35c
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-04-24 12:00:31 +0700

    Fix deploy

--- api/frontendmentor/src/main.rs
+++ api/frontendmentor/src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{web, App, HttpServer};
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Frontendmentor Close" })))
-        .bind(("0.0.0.0", 8083))?
+        .bind(("0.0.0.0", 8084))?
         .run()
         .await
 }
